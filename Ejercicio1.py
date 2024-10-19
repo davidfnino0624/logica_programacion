edec21e75c1f989fa873ffb570864df4ee803f74
davidfnino0624 <davidnino0624@gmail.com>
2024-10-19 12:16:52 -0500

    commit para guardar los comentarios en el primer ejercicio

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -1,1 +1,7 @@
 # https://www.python.org/
+
+"""
+Cuando usamos el # la linea donde se encuentra se vuelve un comentario y como podemos darnos cuenta
+si usamos 3 veces las comillas dobles, vamos a tener la posibilidad de poner un comentario en mas de 
+una sola linea
+"""
