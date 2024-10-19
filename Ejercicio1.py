57d3f9b8898ed0f2d83a0a6f554691a53c8f8e3e
davidfnino0624 <davidnino0624@gmail.com>
2024-10-19 13:06:42 -0500

    finalizacion del primer ejecicio

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -5,3 +5,15 @@ Cuando usamos el # la linea donde se encuentra se vuelve un comentario y como po
 si usamos 3 veces las comillas dobles, vamos a tener la posibilidad de poner un comentario en mas de 
 una sola linea
 """
+my_variable = "Esta variable es un string"
+my_variable = "Esta variable puede cambiar muy facil"
+
+MY_CONSTANT = "Esta es una contante"
+MY_CONSTANT = "Aunque es una constante tambien podemos cambiar su valor con otra declaracion"
+
+my_int = 24
+my_float = 1.75
+my_bool = True
+my_string = "Hola mundo"
+
+print("Hola Phyton")
