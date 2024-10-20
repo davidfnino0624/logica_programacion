7346824ca9dd0b41d7b837f72a6e923571844ada
davidfnino0624 <davidnino0624@gmail.com>
2024-10-20 11:00:47 -0500

    operadores arismeticos basicos

---
+++ Ejercicio01.py
@@ -0,0 +1,10 @@
+"""
+OPERADORES
+"""
+
+# Operadores arismeticos
+
+print(f"Suma: 10 + 3 = {10 + 3}")
+print(f"Resta: 25 - 15 = {25 - 15}")
+print(f"Mutiplicacion: 5 x 8 = {5 * 8}")
+print(f"Division: 25 / 2 = {25 / 2}")
