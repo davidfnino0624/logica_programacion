5b499e47e63326f4fff221154c64a2ccd521fd63
davidfnino0624 <davidnino0624@gmail.com>
2024-10-20 12:31:04 -0500

    Finalizacion de operadores de comparacion

--- Ejercicio01.py
+++ Ejercicio01.py
@@ -8,3 +8,12 @@ print(f"Suma: 10 + 3 = {10 + 3}")
 print(f"Resta: 25 - 15 = {25 - 15}")
 print(f"Mutiplicacion: 5 x 8 = {5 * 8}")
 print(f"Division: 25 / 2 = {25 / 2}")
+print(f"Modulo: 27 % 5 = {27 % 5}")
+print(f"Exponente: 5 ^ 2 = {5 ** 2}")
+print(f"Division entera: 10 / 3 = {10 // 3}")
+
+#Operadores de comparación
+print(f"Igualdad: 10 == 3 es {10 == 3}")
+print(f"Desigualdad: 10 == 3 es {10 != 3}")
+print(f"Mayor que: 10 > 3 es {10 > 3}")
+print(f"Menor que: 10 < 3 es {10 < 3}")
